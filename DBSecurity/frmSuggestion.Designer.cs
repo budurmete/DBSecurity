816456c983810dca76e2bf663dd6b3713e76df1c
budurmete <budurmete@gmail.com>
2022-02-24 19:00:25 +0300

    Proje dosyası ekle.

---
+++ DBSecurity/frmSuggestion.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DBSecurity
+{
+    partial class frmSuggestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNameSurname = new System.Windows.Forms.TextBox();
+            this.txtSuggestion = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name Surname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Suggestion:";
+            // 
+            // txtNameSurname
+            // 
+            this.txtNameSurname.Location = new System.Drawing.Point(229, 12);
+            this.txtNameSurname.Name = "txtNameSurname";
+            this.txtNameSurname.Size = new System.Drawing.Size(263, 37);
+            this.txtNameSurname.TabIndex = 2;
+            // 
+            // txtSuggestion
+            // 
+            this.txtSuggestion.Location = new System.Drawing.Point(12, 102);
+            this.txtSuggestion.Name = "txtSuggestion";
+            this.txtSuggestion.Size = new System.Drawing.Size(864, 519);
+            this.txtSuggestion.TabIndex = 3;
+            this.txtSuggestion.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(576, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send Suggestion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtServerIP.Location = new System.Drawing.Point(12, 671);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(88, 23);
+            this.txtServerIP.TabIndex = 5;
+            this.txtServerIP.Text = "192.168.84.131";
+            // 
+            // frmSuggestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1216, 723);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSuggestion);
+            this.Controls.Add(this.txtNameSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmSuggestion";
+            this.Text = "Suggestion Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNameSurname;
+        private System.Windows.Forms.RichTextBox txtSuggestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtServerIP;
+    }
+}
